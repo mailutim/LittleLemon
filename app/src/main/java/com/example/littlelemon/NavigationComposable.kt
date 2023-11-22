3b807c0be93ed37036188a8929f0dc0c406493a9
mailutim <tim.mailu@gmail.com>
2023-11-22 19:58:08 +0300

    create navigation logic

---
+++ app/src/main/java/com/example/littlelemon/NavigationComposable.kt
@@ -0,0 +1,24 @@
+package com.example.littlelemon
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun NavigationComposable(navController: NavHostController){
+    val hasUserData = hasUserDataInSharedPreference()
+    NavHost(navController = navController, startDestination = if (hasUserData) OnboardDestination.route else HomeDestinations.route ){
+
+    }
+}
+
+@Composable
+fun hasUserDataInSharedPreference(): Boolean {
+    val context = LocalContext.current
+    val sharedPreferences = context.getSharedPreferences(USER_PROFILE, Context.MODE_PRIVATE)
+    val email = sharedPreferences.getString(EMAIL, "") ?: ""
+    return email.isNotBlank()
+}
