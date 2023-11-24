9eea2b842402b21e47f3984ec8bd01f731bc7434
mailutim <tim.mailu@gmail.com>
2023-11-25 00:31:57 +0300

    Add a README for the project

--- app/src/main/java/com/example/littlelemon/NavigationComposable.kt
+++ app/src/main/java/com/example/littlelemon/NavigationComposable.kt
@@ -13,7 +13,7 @@ fun NavigationComposable(navController: NavHostController, database: AppDatabase
 
     NavHost(
         navController = navController,
-        startDestination = if (hasUserData) OnboardingDestination.route else HomeDestination.route
+        startDestination = OnboardingDestination.route
     ) {
         composable(OnboardingDestination.route) { Onboarding(navController = navController) }
         composable(HomeDestination.route) { Home(navController = navController, database) }
